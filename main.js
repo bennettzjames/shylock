2e7d55ecd189cc6bc43a04e0b99a71f412d6b365
bennettzjames <bennettzjames@gmail.com>
2015-11-04 14:53:09 -0500

    got user input to work

--- main.js
+++ main.js
@@ -11,43 +11,19 @@ $prompt = $('#prompt')
 //take the value of prompt and append to the log as a new li
 //then clear
 
-var promptToLog = function(event){
+promptToLog = function(event){
 	var key = event.keyCode;
 	if (key === 13){
 	$promptLi = $('<li>');
 	$promptLi.text($prompt.val());
 	$log.append($promptLi);
-	$promptLi.addClass('command')
+	
 
 }
 
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-// var returnPrompt = function(){
-// 	var promptList = document.elementCreate("li");
-// 	promptList = $prompt.text();
-// 	$log.append();
-
-	
-// $prompt.on('keypress', function(){
-// 	if (keyCode === 13){
-// 		returnPrompt();
-// 	}
-// })
-// };
-
+$prompt.on('keypress', promptToLog);
 
 
 	
